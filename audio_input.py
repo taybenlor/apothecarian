5884cd607c1439bf476e40c2146e06209fd13631
mehwoot <ramlambmoo@gmail.com>
2010-02-11 14:31:41 +1100

    fuck git

--- audio_input.py
+++ audio_input.py
@@ -5,7 +5,7 @@ from numpy import zeros, linspace, short, fromstring, hstack, transpose, mean, a
 from scipy import fft, concatenate
 from threading import Thread
 import random, time, math
-
+ 
 class InputStream(Thread):
 	FORMAT = pyaudio.paInt16
 	SPECTROGRAM_LENGTH = 100
@@ -27,56 +27,56 @@ class InputStream(Thread):
 		self.iscand = False
 		self.next_beats = []
 		self.expected_dist = -1
-
-		
-		
+ 
+ 
+ 
 	def is_a_beat(self, samples, time_offset):
 		avg = mean(absolute(samples))
 		if (avg > self.max):
 			self.max = avg
-
-			
+ 
+ 
 		self.max = self.max * 0.9997
-			
+ 
 		isa_beat = False
-		
-			
+ 
+ 
 		if (avg < self.last_avg and self.last_avg > (self.max * 0.85) and self.iscand):
 			isa_beat = True
 			self.iscand = False
 			self.candidates.append((avg, time_offset))
-			
+ 
 			#now = time.time() - time_offset
 			#dist = now - self.last_beat
 			#self.last_beat = now
 			#self.beats.append(dist)
 			#print (1/median(self.beats))*60
-			
+ 
 		else:
 			if (avg > self.last_avg):
 				self.iscand = True
 			else:
 				self.iscand = False
-		
-		
+ 
+ 
 		self.last_avg = avg
 		return isa_beat
-		
+ 
 	def run(self):
 		pa = pyaudio.PyAudio()
 		stream = pa.open(format=pyaudio.paInt16, channels=1,
 						  rate=InputStream.SAMPLING_RATE,
 						  input=True, frames_per_buffer=InputStream.NUM_SAMPLES)
-						
+ 
 		while not self.closed:
-
+ 
 			self.audio_data = fromstring(stream.read(InputStream.NUM_SAMPLES), dtype=short)/32768.0
 			now = time.time()
 			dist = now - self.last			
 			self.last = now
-			
+ 
 			self.candidates = []
-			
+ 
 			sample_divisor = 8
 			for i in xrange(0,sample_divisor):
 				cur = self.audio_data[i * (1024/sample_divisor):((i+1)*(1024/sample_divisor))-1]
@@ -88,14 +88,14 @@ class InputStream(Thread):
 					pass
 			local_max = -1
 			local_offset = -1
-
+ 
 			for each in self.candidates:
 				if (each[0] > local_max):
 					local_max = each[0]
 					local_offset = each[1]
-			
+ 
 			self.expected_dist = median(self.beats)
-					
+ 
 			if (local_max != -1):
 				now = time.time() - local_offset
 				dist = now - self.last_beat
@@ -103,12 +103,13 @@ class InputStream(Thread):
 				self.beats.append(dist)
 				# If this is a good beat
 				if (abs(dist - self.expected_dist) < 0.04):
+					self.next_beats = []
 					self.next_beats.append(now + self.expected_dist)
-					
-				
+ 
+ 
 			energy = self.get_average_energy()
-			
-			
+ 
+ 
 			if self.history != None:
 				#print self.history
 				self.history = concatenate((self.history, [absolute(energy)]))
@@ -116,32 +117,32 @@ class InputStream(Thread):
 					self.history = self.history[(len(self.history) - (InputStream.NUM_SAMPLES * 120)):]
 			else:
 				self.history = [absolute(energy)]
-			
+ 
 			if len(self.beats) > 100:
 				self.beats = self.beats[10:]			
 			#hist = mean(self.history)
-			
+ 
 			bpm = self.get_bpm()
-
-
-			
+ 
+ 
+ 
 			p_bpm = 80
 			if len(self.beats) >=10:
 				p_bpm = 2/((now - self.beats[-1])/60)
-				
+ 
 			#if self.get_average_energy() > 1.3*hist:
 			#if self.get_average_energy() > (0.75 * self.max):
 			#	self.beat = True
 			#	self.beats.append(dist)
-
+ 
 			#print self.beats
-
-
+ 
+ 
 		stream.close()
-	
+ 
 	def close(self):
 		self.closed = True
-		
+ 
 	def get_bpm(self):
 		if len(self.beats) < 2:
 			return 0
@@ -149,21 +150,21 @@ class InputStream(Thread):
 		end = self.beats[-1]
 		mins = (end-start)/60
 		return len(self.beats)/mins
-	
+ 
 	def get_audio_spectrum(self, buckets=100):
 		if self.audio_data == None:
 			return [0]*100
 		return abs(fft(self.audio_data, 2*buckets))[:buckets]
-	
+ 
 	def get_average_energy(self):
 		if self.audio_data == None:
 			return 0
 		return mean(absolute(self.audio_data))
-	
+ 
 	def is_beat(self):
 		if (len(self.next_beats) == 0):
 			return False
-		if (time.time() > self.next_beats[0]):
+		if (time.time() > (self.next_beats[0]-0.10)):
 			last = self.next_beats[0]
 			self.next_beats = self.next_beats[1:]
 			if (len(self.next_beats) == 0):
--- visualisation.py
+++ visualisation.py
@@ -1,7 +1,7 @@
 from pymt import *
 from audio_input import InputStream
 import random, time, math
-
+ 
 class Visualisation(MTWidget):
 	#-----------------
 	# Give it a stream if you have one already, otherwise it will create its own.
@@ -13,38 +13,38 @@ class Visualisation(MTWidget):
 			self.stream = InputStream()
 			self.stream.start()
 		self._last_tick = time.time()
-	
+ 
 	def draw(self):
 		temp = time.time()
 		self.visualise((temp-self._last_tick)*1000)
 		self._last_tick = temp
-	
+ 
 	#------------------
 	# Override this method to make your own visualisation
 	# ticks is millis since last call (float)
 	#------------------
 	def visualise(self, ticks):
 		pass
-		
+ 
 class BasicVisualisation(Visualisation):
 	def __init__(self, **kwargs):
 		super(BasicVisualisation, self).__init__(**kwargs)
 		self.bg_color = (0.0, 0.0, 0.0)
-	
+ 
 	def visualise(self, ticks):
 		#draw the background		
 		graphx.colors.set_color(*self.bg_color)
 		graphx.draw.drawRectangle((0,0), (self.width, self.height))
-		
+ 
 		#get our visualisation values
 		val = self.stream.get_average_energy() # 0.0 -> 1.0
 		spectrum = self.stream.get_audio_spectrum() # array of floats (each value is contribution, takes optional number of buckets)
-		
+ 
 		#draw the foreground
 		graphx.colors.set_color(1.0,1.0,1.0)
 		#circle in the middle of the widget with radius val* (1/3 of screen) means circle can only be as bit as 2/3rds screen
 		graphx.draw.drawCircle(self.center, val * (self.width/3))
-		
+ 
 		#draw points evenly across the screen
 		points = []
 		x_mul = self.width/len(spectrum)
@@ -52,24 +52,24 @@ class BasicVisualisation(Visualisation):
 			points.append(i*x_mul) #x coordinate of point
 			points.append(item*150) #y coordinate of point (ie height = spectrum contribution)
 		graphx.draw.drawLine(points)
-		
+ 
 		#if its a beat randomly change the background colour
 		if self.stream.is_beat():
 			self.bg_color = (random.random(), random.random(), random.random())
-		
+ 
 class CircleVisualisation(Visualisation):
 	def __init__(self, **kwargs):
 		super(CircleVisualisation, self).__init__(**kwargs)
 		self.fg_color = (1.0, 1.0, 1.0)
-		
+ 
 	def visualise(self, ticks):
 		graphx.colors.set_color(0.0,0.0,0.0)
 		graphx.draw.drawRectangle((self.x,self.y), (self.width, self.height))
-		
+ 
 		graphx.colors.set_color(*self.fg_color)
 		val = self.stream.get_average_energy() * (self.width/3)
 		graphx.draw.drawCircle(self.center, val)
-		
+ 
 		#arrange the points around in a circle
 		cx, cy = self.center
 		length = val
@@ -79,85 +79,86 @@ class CircleVisualisation(Visualisation):
 		for d in spectrum:
 			lx = ((d * 75) +val) * math.cos(angle)
 			ly = ((d * 75)+val) * math.sin(angle)
-
+ 
 			points.append(cx+lx)
 			points.append(cy+ly)
-			
+ 
 			angle += math.pi*2/(len(spectrum))
-		
+ 
 		#complete the circle
 		points.append(points[0])
 		points.append(points[1])
 		graphx.draw.drawLine(points)
-		
+ 
 		#if its a beat change the fg colour
 		if self.stream.is_beat():
 			self.fg_color = (random.random(), random.random(), random.random())
-
+ 
 PARTICLE_LIMIT = 200
 class ParticleVisualisation(Visualisation):
 	def __init__(self, **kwargs):
 		super(ParticleVisualisation, self).__init__(**kwargs)
 		self.particles = {}
 		self.uid = 0
-		
+ 
 	def visualise(self, ticks):
 		graphx.colors.set_color(0.0, 0.0, 0.0)
 		graphx.draw.drawRectangle((self.x,self.y), (self.width, self.height))
-		
+ 
 		cx, cy = self.center
 		angle = 0
 		spectrum = self.stream.get_audio_spectrum()
-
+ 
 		if len(self.particles) < PARTICLE_LIMIT:
 			for d in spectrum:
 				count = int(round(d))
-			
+ 
 				dx = math.cos(angle)
 				dy = math.sin(angle)
-
+ 
 				x = cx + random.randint(-10, 10)
 				y = cy + random.randint(-10, 10)
-			
+ 
 				dx *= 20
 				dy *= 20
-			
+ 
 				#for i in xrange(count):
 				if count > 1:
 					self.particles[self.uid] = Particle(x, y, dx, dy)
 					self.uid += 1
-			
+ 
 				angle += math.pi*2/(len(spectrum))
-			
+ 
 		for p in self.particles.keys():
 			self.particles[p].draw()
 			self.particles[p].update()
 			x, y = self.particles[p].x, self.particles[p].y
 			if x < 0 or y < 0 or x > self.width or y > self.height:
 				self.particles.pop(p)
-				
-
+ 
+ 
 class Particle(object):
 	def __init__(self, x, y, dx, dy, color=None, radius=None):
 		self.color = color
 		if self.color == None:
 			self.color = (random.random(), random.random(), random.random())
-		
+ 
 		self.radius = radius
 		if self.radius == None:
 			self.radius = random.randint(1,10)
-		
+ 
 		self.x = x
 		self.y = y
 		self.dx = dx
 		self.dy = dy
 		self.alive = 0
-
+ 
 	def update(self):
 		self.x += self.dx
 		self.y += self.dy
 		self.alive += 1
-
+ 
 	def draw(self):
 		graphx.colors.set_color(*self.color)
 		graphx.draw.drawCircle((int(self.x), int(self.y)), self.radius)
+ 
