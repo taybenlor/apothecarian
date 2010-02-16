21d70c22f5d7d1afcfdad6e862d0594533a67f38
Ben Taylor <xachro@gmail.com>
2010-02-16 22:49:38 +1100

    Added interaction to the particle visualisation, just click on the screen.

--- visualisation.py
+++ visualisation.py
@@ -105,12 +105,24 @@ class ParticleVisualisation(Visualisation):
 		for i in xrange(self.max):
 			self.particles.append(Particle())
 		self.count = 0
+		self.gravity_wells = {}
 		
+	def on_touch_down(self, touch):
+		self.gravity_wells[touch.id] = (touch.x, touch.y)
+		touch.grab(self)
+		return True
+
+	def on_touch_move(self, touch):
+		self.gravity_wells[touch.id] = (touch.x, touch.y)
+		return True
+
+	def on_touch_up(self, touch):
+		if touch.id in self.gravity_wells:
+			self.gravity_wells.pop(touch.id)
+		return True
  
 	def visualise(self, ticks):
 		self.count += ticks
-		graphx.colors.set_color(0.0, 0.0, 0.0)
-		graphx.draw.drawRectangle((self.x,self.y), (self.width, self.height))
  
 		cx, cy = self.center
 		angle = 0
@@ -143,32 +155,51 @@ class ParticleVisualisation(Visualisation):
 		with DO(blend, gx_enable(self.image.texture.target), gx_enable(GL_POINT_SPRITE_ARB), gx_begin(GL_POINTS)):
 			for particle in self.particles:
 				if particle.visible:
-					particle.update(self.width, self.height)
-					glColor3f(*particle.color)
+					particle.update(self.width, self.height,self.gravity_wells.values())
+					glColor4f(particle.color[0], particle.color[1], particle.color[2], particle.alpha)
 					glVertex2f(particle.x, particle.y)
 				elif len(todo):
 					particle.animate(*todo.pop(-1))
 
-				
  
+GRAVITY_WELL_FORCE = 400000
 class Particle(object):
 	def __init__(self):
 		self.visible = False
  
-	def update(self, wx, wy):
+	def update(self, wx, wy, gravity_wells=[]):
+		self.update_velocity(gravity_wells)
 		self.x += self.dx
 		self.y += self.dy
+		self.alpha -= 0.02
+		if self.alpha <= 0.0:
+			self.visible = False
 		if self.x > wx or self.x < 0:
 			self.visible = False
 		elif self.y > wy or self.y < 0:
 			self.visible = False
 		
+	def update_velocity(self, gravity_wells=[]):
+		if not gravity_wells:
+			return 
+		for wx,wy in gravity_wells:
+			distance = ((self.x - wx)**2 + (self.y - wy)**2)**0.5
+			ndx = (wx-self.x)/(distance+0.01)
+			ndy = (wy-self.y)/(distance+0.01)
+			gf = GRAVITY_WELL_FORCE*((1/(distance+0.01))**2)
+			fdx = ndx*gf
+			fdy = ndy*gf
+			self.dx += fdx
+			self.dy += fdy
+
 	def animate(self, x, y, dx, dy):
 		self.x = x
 		self.y = y
+		#self.z = random.randint(-100,100)
 		self.dx = dx
 		self.dy = dy
-		self.color = (random.random(), random.random(), random.random())
+		self.color = (random.uniform(0, 1.0), random.uniform(0.0, 1.0), random.uniform(0.0, 1.0))
+		self.alpha = 1.0
 		self.size = (random.randint(10,40))
 		self.visible = True
  
